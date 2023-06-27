50e023f46fbd7329de5730ce2d0d7698bb63c325
Joy Biswas <contact.joybiswas@gmail.com>
2023-06-27 23:06:21 +0600

    Basic Set Up done

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div>Copy right text</div>
+            <div>Other text</div>
+        </div>
+    );
+};
+
+export default Footer;
